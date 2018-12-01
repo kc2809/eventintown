c6b6ef083a1085d883e1d2f9f5147740666b6a41
Duy Le <duyle.bks2014@gmail.com>
2018-12-01 09:21:30 +0700

    firebase config and tablayout

---
+++ danaevent/app/src/main/java/com/bof/devfest18/danaevent/fragments/EventInTownFragment.java
@@ -0,0 +1,21 @@
+package com.bof.devfest18.danaevent.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bof.devfest18.danaevent.R;
+
+public class EventInTownFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.event_in_town_fragment, container, false);
+        return view;
+    }
+}
