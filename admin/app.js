175bb094862149e628f4e967f5fee68301e1693e
Din Din <tridung2110dn@gmail.com>
2018-12-01 14:23:47 +0700

    insert data

--- admin/app.js
+++ admin/app.js
@@ -1,4 +1,4 @@
-(function () {
+$(document).ready(function () {
     const config = {
         apiKey: "AIzaSyD4anhA5e7OK-ytLJbMjyXnudTsqRtGeHE",
         authDomain: "dana-event.firebaseapp.com",
@@ -8,13 +8,39 @@
         messagingSenderId: "219297288084"
     };
     firebase.initializeApp(config);
+    var database = firebase.database();
 
     const preObject = document.getElementById("object");
+    const dbRefObject = database.ref().child("user");
 
-    const dbRefObject = firebase.database().ref().child("user");
-
+    // get data
     dbRefObject.on("value", snap => {
-        preObject.innerText = JSON.stringify(snap.val(), null, 3);
+        preObject.innerText = JSON.stringify(snap.val(), null, 4);
+
     });
 
-}());
+    $("#btnClick").click(function () {
+       insert_user(55, "dung", "admin", "admin@gmail.com", "123");
+    });
+
+    // Retrieve new posts as they are added to our database
+    dbRefObject.on("child_changed", function(data) {
+        console.log(data);
+        preObject.innerText = JSON.stringify(data.val(), null, 10);
+    });
+
+    function insert_user(userId, username, role, email, password) {
+        database.ref('user/' + userId).set({
+            username : username,
+            role : role,
+            email : email,
+            password : password
+        });
+    }
+});
+
+
+
+
+
+
